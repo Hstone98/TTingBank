26c8395c56efe609934f5a7c51d9901a4e0cffb6
hs.go <99238027+Hstone98@users.noreply.github.com>
2023-03-27 15:29:23 +0900

    [flutter] - Controller 분리 및 폴더 구조 변경.

--- tting_bank/lib/main.dart
+++ tting_bank/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tting_bank/MainPage.dart';
+import 'package:flutter/material.dart';
+import 'package:tting_bank/view/MainPage.dart';
 
 void main() {
   runApp(
