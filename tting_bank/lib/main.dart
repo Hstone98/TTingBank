d37f59ca5c71147b52cce3d4847059cac88e9ba4
hs.go <99238027+Hstone98@users.noreply.github.com>
2023-03-27 15:39:20 +0900

    [flutter] - flutter 표기법으로 클래스명, 파일명 변경.

--- tting_bank/lib/main.dart
+++ tting_bank/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
-import 'package:tting_bank/view/MainPage.dart';
+import 'package:tting_bank/view/main_page.dart';
 
 void main() {
   runApp(
