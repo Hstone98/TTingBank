47d3cedb5266568546cc6377b5634e75e674af35
hs.go <hs.go@gogotech.biz>
2023-04-05 11:37:17 +0900

    [flutter] - merge.

--- tting_bank/lib/main.dart
+++ tting_bank/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:tting_bank/view/assetmanagement_page.dart';
+import 'package:tting_bank/view/cardInfo_page.dart';
+import 'package:tting_bank/view/recommend_page.dart';
+import 'package:tting_bank/view/assetmanagement_page.dart';
 import 'package:flutter/material.dart';
 import 'kakaologin/kakao_init.dart';
 import 'package:tting_bank/view/main_page.dart';
+import 'package:tting_bank/view/store_list_page.dart';
+import 'package:tting_bank/view/cashback_this_month_tting.dart';
+import 'package:tting_bank/view/consumption_details.dart';
+
 import 'view/login_page.dart';
 import 'view/setting_page.dart';
 
@@ -9,7 +17,8 @@ void main() {
   KakaoInit();
   runApp(
     MaterialApp(
-      home: SettingPage(),
+      home: CashbackThisMonthTting(),
+      home: AssetmanagementPage(),
     ),
   );
 }
--- tting_bank/lib/view/main_page.dart
+++ tting_bank/lib/view/main_page.dart
@@ -6,6 +6,103 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        
+        iconTheme: IconThemeData(
+            color: Colors.black,//색변경
+          ),
+        backgroundColor: Colors.white,
+        leading: Icon(Icons.menu),
+        title: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+          child: Row(
+            children: [
+          Icon(Icons.search),
+          ],
+          ),
+        ),
+        
+      ),
+
+      body: Padding(
+        padding: EdgeInsetsDirectional.fromSTEB(30, 50, 0, 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 0, 7, 0),
+                  child: Image.asset(
+                    'bankTting/img/testimg1.png', width: 70, height: 70
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(7, 0, 7, 0),
+                  child: Image.asset(
+                    'bankTting/img/testimg2.png', width: 70, height: 70
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(7, 0, 7, 0),
+                  child: Image.asset(
+                    'bankTting/img/testimg3.png', width: 70, height: 70
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(7, 0, 0, 0),
+                  child: Image.asset(
+                    'bankTting/img/testimg4.png', width: 70, height: 70
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 20, 7, 40),
+                  child: Image.asset(
+                    'bankTting/img/testimg5.png', width: 70, height: 70
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(7, 20, 7, 40),
+                  child: Image.asset(
+                    'bankTting/img/testimg6.png', width: 70, height: 70
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(7, 20, 7, 40),
+                  child: Image.asset(
+                    'bankTting/img/testimg7.png', width: 70, height: 70
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(7, 20, 0, 40),
+                  child: Image.asset(
+                    'bankTting/img/testimg8.png', width: 70, height: 70
+                  ),
+                ),
+              ],
+            ),
+            Row(children: [
+              Container( height:1.0,
+           width:500.0,
+           color:Colors.black,)
+            ],
+            ),
+            Row(children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(20, 50, 0, 0),
+                child: Image.asset(
+                  'bankTting/img/testcard.png', width: 300, height: 220
+                ),
+              )
+            ],)
+          ],
+        ),
+      ),
+
+      );
   }
 }
