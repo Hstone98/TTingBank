ae0ffc82bf4e58e6aec3d82f2be5c28c810d05d9
LEEEEEEHS <lwangb2@naver.com>
2023-04-13 23:50:08 +0900

    [flutter] - 여행 카테고리 추가로 인한 TODO 주석 추가

--- Source/Client/tting_bank/lib/view/store_list_page.dart
+++ Source/Client/tting_bank/lib/view/store_list_page.dart
@@ -53,34 +53,38 @@ class _storeListPage extends State<StoreListPage>
             backgroundColor: Colors.white,
             title: Text(title, style: TextStyle(color: Colors.black)),
             bottom: TabBar(
-            indicatorColor: Colors.black,
-            indicatorWeight: 2.0,
-            isScrollable: true,
-            controller: _tabController,
-            tabs: [
-              //TODO: 배열 생성해서 동적으로 만들어지도록 구현.
-              //TODO: 카테고리 내용은 데이터베이스에서 가져오도록 구현.
-              Tab(
-                child: Text('카페', 
-                  style: TextStyle(color: Colors.black)
-                ),
-              ),
-              Tab(
-                child: Text('영화', 
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-                Tab(
-                child: Text('편의점', 
-                  style: TextStyle(color: Colors.black)
-                ),
-              ),
-            ]),
+                indicatorColor: Colors.black,
+                indicatorWeight: 2.0,
+                isScrollable: true,
+                controller: _tabController,
+                tabs: [
+                  //TODO: 배열 생성해서 동적으로 만들어지도록 구현.
+                  //TODO: 카테고리 내용은 데이터베이스에서 가져오도록 구현.
+                  //TODO: 여행 카테고리 추가 및 메인페이지에서 여행 누르면 이동하도록 수정
+                  Tab(
+                    child: Text('카페', style: TextStyle(color: Colors.black)),
+                  ),
+                  Tab(
+                    child: Text(
+                      '영화',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  Tab(
+                    child: Text('편의점', style: TextStyle(color: Colors.black)),
+                  ),
+                ]),
           ),
           body: TabBarView(controller: _tabController, children: [
-            Container(color: Colors.grey[300], child: StoreListScreen(ImageCategory.listStoreCafe)),
-            Container(color: Colors.grey[300], child: StoreListScreen(ImageCategory.listStoreMovie)),
-            Container(color: Colors.grey[300], child: StoreListScreen(ImageCategory.listStoreConvenience))
+            Container(
+                color: Colors.grey[300],
+                child: StoreListScreen(ImageCategory.listStoreCafe)),
+            Container(
+                color: Colors.grey[300],
+                child: StoreListScreen(ImageCategory.listStoreMovie)),
+            Container(
+                color: Colors.grey[300],
+                child: StoreListScreen(ImageCategory.listStoreConvenience))
             // Container(color: Colors.grey[300], child: StoreListScreen(ImageCategory.listStoreCafe)),
             // Container(color: Colors.grey[300], child: StoreListScreen(ImageCategory.listStoreCafe)),
             // Container(color: Colors.grey[300], child: StoreListScreen(ImageCategory.listStoreCafe))
@@ -105,7 +109,7 @@ class StoreListScreen extends StatelessWidget {
             mainAxisSpacing: 10,
             crossAxisCount: 4,
             children: <Widget>[
-              for(int i = 0; i < listStore!.length; i++)
+              for (int i = 0; i < listStore!.length; i++)
                 createColumn(listStore!, i, context),
             ],
           ),
