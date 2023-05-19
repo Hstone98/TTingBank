3aeffdae32261e0be742a338d493864c99f4381b
hs.go <99238027+Hstone98@users.noreply.github.com>
2023-05-19 19:08:59 +0900

    [flutter] - 카드 추천 페이지 -> 미보유 카드 동적할당 코드 추천.
    구현한 내용
    - 미보유 카드 동적할당 코드 구현.
    
    구현 필요한 내용.
    - 화면 생성 시, DB에서 보유 카드, 미보유 카드 개수 가져오기.
    - 미보유 카드 동적할당 코드 보고 보유 카드 동적할당 기능 구현 필요.
    -

--- Source/Client/tting_bank/lib/view/recommend_page.dart
+++ Source/Client/tting_bank/lib/view/recommend_page.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:tting_bank/data/img_card.dart';
 
-//카드추천 페이지
+//------------------------------------------------------------------------------------------------//
+//
+//------------------------------------------------------------------------------------------------//
+// TODO : 보유카드와 추천카드 개수는 DB에서 count 데이터 가지고 와서 설정 -> 화면 전환 시, 페이지 로드하면서 가지고 오기.
+var possessCardIndex = 0; // 보유카드 index
+var suggestCardIndex = 0; // 추천카드 index
+
+//------------------------------------------------------------------------------------------------//
+// 카드 추천 페이지
+//------------------------------------------------------------------------------------------------//
 class RecommendPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-    length: 4,
+      length: 4,
       child: Scaffold(
         backgroundColor: Color.fromARGB(255, 236, 243, 244),
         appBar: AppBar(
@@ -28,76 +37,67 @@ class RecommendPage extends StatelessWidget {
           ),
           centerTitle: true,
           bottom: TabBar(
-            tabs: TABS.map(
-              (e) => Tab(
-                child: Text(
-                  e.label,style: TextStyle(color: Colors.black),
-                ),
-              )
-            ).toList()
-          ),
+              tabs: TABS
+                  .map((e) => Tab(
+                        child: Text(
+                          e.label,
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ))
+                  .toList()),
         ),
         body: SafeArea(
           child: Column(
             children: [
               SingleChildScrollView(
-                
                 child: Column(
-                   mainAxisSize: MainAxisSize.max,
+                  mainAxisSize: MainAxisSize.max,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(16, 20, 0, 0),
-                child: Text(
-                  '보유중인 카드', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[500],),
-                )
-              ),
+                        padding: EdgeInsetsDirectional.fromSTEB(16, 20, 0, 0),
+                        child: Text(
+                          '보유중인 카드',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[500],
+                          ),
+                        )),
+                    // TODO : 보유카드 동적으로 만들기 -> 미보유 카드 동적할당 코드 참조.
                     Card(
-                      child: Createcardinfo(
-                      ),
+                      child: CreateCardInfo(),
                     ),
                     Card(
-                      child: Createcardinfo(
-                      ),
+                      child: CreateCardInfo(),
                     ),
                     Card(
-                      child: Createcardinfo(
-                      ),
+                      child: CreateCardInfo(),
                     ),
                   ],
                 ),
               ),
-        
               SingleChildScrollView(
                 child: Column(
-                   mainAxisSize: MainAxisSize.max,
+                  mainAxisSize: MainAxisSize.max,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(16, 20, 0, 0),
-                child: Text(
-                  '미 보유 카드 추천', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[500],),
-                )
-              ),
-                    Card(
-                      child: Createcardinfo(
+                        padding: EdgeInsetsDirectional.fromSTEB(16, 20, 0, 0),
+                        child: Text(
+                          '미 보유 카드 추천',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[500],
+                          ),
+                        )),
+                    // TODO : 미보유 카드 추천 동적 할당 코드.
+                    for (int i = 0; i < suggestCardIndex; i++)
+                      Card(
+                        child: CreateCardInfo(),
                       ),
-                    ),
-                    Card(
-                      child: Createcardinfo(
-                      ),
-                    ),
-                    Card(
-                      child: Createcardinfo(
-                      ),
-                    ),
                   ],
                 ),
               ),
-        
-        
-        
-              
             ],
           ),
         ),
@@ -106,79 +106,77 @@ class RecommendPage extends StatelessWidget {
   }
 }
 
-class Createcardinfo extends StatelessWidget{
-Widget build(BuildContext context){
-  return Row(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Expanded(
-            child: Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(8, 4, 0, 4),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
-                    child: Text(
-                      'NH 올바른 FLEX',style: TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),)
-                    ),
+//------------------------------------------------------------------------------------------------//
+//
+//------------------------------------------------------------------------------------------------//
+class CreateCardInfo extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Expanded(
+          child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(8, 4, 0, 4),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 15, 0, 0),
+                  child: Text('NH 올바른 FLEX',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      )),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                  child: Text('NH 올바른 FLEX',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                  child: Text(
+                    'twosome place 10%할인',
                   ),
-                  Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                    child: Text(
-                      'NH 올바른 FLEX',style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                    child: Text(
-                      'twosome place 10%할인',
-                     
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
-                    child: Text(
-                      'NH 올바른 FLEX',style: TextStyle(
-            color: Color.fromARGB(255, 255, 255, 255),)
-                    ),
-                  ),
-                  
-                ],
-              ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                  child: Text('NH 올바른 FLEX',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      )),
+                ),
+              ],
             ),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(0),
-              bottomRight: Radius.circular(20),
-              topLeft: Radius.circular(0),
-              topRight: Radius.circular(20),
-            ),
-            child: Image.asset(
-                    ImgCard.listCard[0],
-                    width: 120,
-                    height: 60,
-                  ),
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(0),
+            bottomRight: Radius.circular(20),
+            topLeft: Radius.circular(0),
+            topRight: Radius.circular(20),
           ),
-        ],
-      );
+          child: Image.asset(
+            ImgCard.listCard[0],
+            width: 120,
+            height: 60,
+          ),
+        ),
+      ],
+    );
+  }
 }
+
+class TabInfo {
+  final String label;
+
+  const TabInfo({required this.label});
 }
 
-
-class TabInfo{
-  final String label; 
-
-  const TabInfo({
-    required this.label
-  });
-
-}
-const TABS= [
+const TABS = [
   TabInfo(label: '할인금액 순'),
   TabInfo(label: '할인률 순'),
   TabInfo(label: '적립금 순'),
