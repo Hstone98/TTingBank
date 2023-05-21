0d0b6211fddadb8bd582fa36a21b516e7929ac70
DESKTOP-B20HPTP\kyoun <kyoungae1231@naver.com>
2023-05-21 16:00:02 +0900

    [flutter] - 소비내역 화면 구성

--- Source/Client/tting_bank/lib/view/cashback_this_month_tting.dart
+++ Source/Client/tting_bank/lib/view/cashback_this_month_tting.dart
@@ -1,17 +1,122 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:flutter/cupertino.dart';
 
-class CashbackThisMonthTting extends StatelessWidget {
+class CashbackThisMonthTting extends StatefulWidget {
   const CashbackThisMonthTting({Key? key}) : super(key: key);
 
   @override
+  _CashbackThisMonthTtingState createState() => _CashbackThisMonthTtingState();
+}
+
+class _CashbackThisMonthTtingState extends State<CashbackThisMonthTting> {
+  int year = DateTime.now().year;
+  int month = DateTime.now().month;
+  List<int> years =
+      List.generate(DateTime.now().year - 2019, (index) => 2020 + index);
+  List<int> months = List.generate(12, (index) => index + 1);
+
+  void decreaseMonth() {
+    setState(() {
+      if (month > 1) {
+        month--;
+      } else {
+        month = 12;
+        year--;
+      }
+    });
+  }
+
+  void increaseMonth() {
+    setState(() {
+      if (month < 12) {
+        month++;
+      } else {
+        month = 1;
+        year++;
+      }
+    });
+  }
+
+  Future<void> showMonthPicker(BuildContext context) async {
+    await showDialog<int>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0), // 모서리를 둥글게 조정
+          ),
+          title: Text('년도 선택'),
+          content: Container(
+            height: MediaQuery.of(context).size.height / 2, // 화면 높이의 절반
+            width: double.maxFinite,
+            child: ListView.builder(
+              shrinkWrap: true, // 리스트뷰 크기를 내용에 맞게 조정
+              physics: BouncingScrollPhysics(), // 스크롤 효과 적용
+              itemCount: years.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(years[index].toString()),
+                  onTap: () {
+                    setState(() {
+                      year = years[index];
+                      Navigator.of(context).pop();
+                    });
+                  },
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
+
+    await showDialog<int>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0), // 모서리를 둥글게 조정
+          ),
+          title: Text('월 선택'),
+          content: Container(
+            height: MediaQuery.of(context).size.height / 2, // 화면 높이의 절반
+            width: double.maxFinite,
+            child: ListView.builder(
+              shrinkWrap: true, // 리스트뷰 크기를 내용에 맞게 조정
+              physics: BouncingScrollPhysics(), // 스크롤 효과 적용
+              itemCount: months.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(months[index].toString()),
+                  onTap: () {
+                    setState(() {
+                      month = months[index];
+                      Navigator.of(context).pop();
+                    });
+                  },
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    bool canDecrease = year > 2020 || month > 1;
+    bool canIncrease =
+        year < DateTime.now().year || month < DateTime.now().month;
+
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(
-          color: Colors.black, //색변경
+          color: Colors.black, // 아이콘 색상 변경
         ),
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
         leading: IconButton(
           icon: Icon(
             Icons.arrow_back,
@@ -21,131 +126,69 @@ class CashbackThisMonthTting extends StatelessWidget {
             Navigator.of(context).pop();
           },
         ),
-        title: Text('소비 내역', style: TextStyle(color: Colors.black)),
+        title: Text(
+          '소비 내역',
+          style: TextStyle(color: Colors.black),
+        ),
       ),
-      body: SingleChildScrollView(
-        //오버플로우 나는거 해결
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(25, 50, 0, 20),
-                  child: Text('이번달 소비액',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold)),
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_left,
+                    color: canDecrease
+                        ? Colors.black
+                        : Colors.grey, // 비활성화일 때 회색으로 변경
+                  ),
+                  onPressed: canDecrease ? decreaseMonth : null,
+                ),
+                GestureDetector(
+                  onTap: () => showMonthPicker(context),
+                  child: Text(
+                    '$year년 $month월',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_right,
+                    color: canIncrease
+                        ? Colors.black
+                        : Colors.grey, // 비활성화일 때 회색으로 변경
+                  ),
+                  onPressed: canIncrease ? increaseMonth : null,
                 ),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(200, 10, 20, 0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    hintText: '--금액 출력란--',
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        borderSide: BorderSide(color: Colors.white))),
-              ),
+          ),
+          Expanded(
+            child: ListView.separated(
+              itemCount: 20, // 예시로 20개의 소비 기록을 표시
+              separatorBuilder: (context, index) => Divider(), // 구분선 추가
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('소비 기록 $index'),
+                  subtitle: Text('소비 내용'),
+                  trailing: Text(
+                    '-\1000원',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ), // 소비 금액
+                );
+              },
             ),
-            Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(18, 50, 0, 0),
-                child: Text('이번달 소비금액',
-                    style: TextStyle(color: Colors.black, fontSize: 15)),
-              ),
-            ]),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(18, 0, 0, 20),
-                  child: Text('1,120,000',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold)),
-                )
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-              child: LinearPercentIndicator(
-                animation: true,
-                animationDuration: 1000,
-                lineHeight: 20,
-                percent: 0.6,
-                progressColor: Colors.red,
-                backgroundColor: Colors.grey.shade200,
-              ),
-            ),
-            Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(18, 50, 0, 0),
-                child: Text('이번달 받은 캐시백',
-                    style: TextStyle(color: Colors.black, fontSize: 15)),
-              ),
-            ]),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(18, 0, 0, 20),
-                  child: Text('120,000',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold)),
-                )
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-              child: LinearPercentIndicator(
-                animation: true,
-                animationDuration: 1000,
-                lineHeight: 20,
-                percent: 0.3,
-                progressColor: Colors.purple,
-                backgroundColor: Colors.grey.shade200,
-              ),
-            ),
-            Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(18, 50, 0, 0),
-                child: Text('이번달 받은 카드혜택',
-                    style: TextStyle(color: Colors.black, fontSize: 15)),
-              ),
-            ]),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(18, 0, 0, 20),
-                  child: Text('220,000',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold)),
-                )
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-              child: LinearPercentIndicator(
-                animation: true,
-                animationDuration: 1000,
-                lineHeight: 20,
-                percent: 0.2,
-                progressColor: Colors.green,
-                backgroundColor: Colors.grey.shade200,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
